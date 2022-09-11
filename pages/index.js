b86cb77320c1c99b76de05b1321e855cd2345ce2
Borislav Stoyanov <bostoy@protonmail.com>
2022-09-11 10:31:21 +0300

    🔥 Reposition the Head to be in _app.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,5 @@
 import Head from "next/head"
 
 export default function Home() {
-    return (
-        <div>
-            <Head>
-                <title>NFT Marketplace</title>
-                <meta name="description" content="NFT Marketplace" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            Home Page
-        </div>
-    )
+    return <div>Home Page</div>
 }
--- pages/sell-nft.js
+++ pages/sell-nft.js
@@ -1,16 +1,7 @@
 import Head from "next/head"
 
 const sellNft = () => {
-    return (
-        <div>
-            <Head>
-                <title>Sell NFT | NFT Marketplace</title>
-                <meta name="description" content="NFT Marketplace" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            Sell NFT
-        </div>
-    )
+    return <div>Sell NFT</div>
 }
 
 export default sellNft
