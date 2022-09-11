1d66fd01f19979096e72cf458a16da4a542ea804
Borislav Stoyanov <bostoy@protonmail.com>
2022-09-11 19:55:32 +0300

    🚀 Connect to the Moralis server

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,10 @@ import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
 import Header from "../components/Header"
 import Head from "next/head"
+
+const NEXT_PUBLIC_APP_ID = process.env.NEXT_PUBLIC_APP_ID
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+
 function MyApp({ Component, pageProps }) {
     return (
         <>
@@ -10,7 +14,7 @@ function MyApp({ Component, pageProps }) {
                 <meta name="description" content="NFT Marketplace" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <MoralisProvider initializeOnMount={false}>
+            <MoralisProvider appId={NEXT_PUBLIC_APP_ID} serverUrl={NEXT_PUBLIC_SERVER_URL}>
                 <Header />
                 <Component {...pageProps} />
             </MoralisProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
 import Head from "next/head"
 
 export default function Home() {
+    //How do we show the recently listed NFTs?
+
+    //We will index the events off-chain and then read from our database.
+    //Setup a server to listen for those events to be fired, and we will add them to a database to query.
+    //The Graph does this in a decentralized way
+    /** Moralis does it in a centralized way and comes with a ton of other functions
+     * All our logic is still 100% on chain.
+     * Speed & Development time.
+     * Its really hard to start a prod blockchain project 100% decentralized.
+     * They are working on open sourcing their code
+     * Feature richness
+     * We can create more features with a centralized back end to start
+     * AS more decentralized tools are being created
+     * Local development
+     */
     return <div>Home Page</div>
 }
