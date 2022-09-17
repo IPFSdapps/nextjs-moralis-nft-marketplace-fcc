20556bb17f0beff66a506bd84870fbfdd07535c5
Borislav Stoyanov <bostoy@protonmail.com>
2022-09-17 18:52:51 +0300

    ✨ Initialize Moralis Cloud Funciton

--- cloudFunctions/updateActiveItems.js
+++ cloudFunctions/updateActiveItems.js
@@ -1,1 +1,10 @@
-console.log("hi")
+//Create a new table called "Active Item"
+//Add items when they are listed on the marketplace
+//Remove them when they are bought or cancelled
+
+Moralis.Cloud.afterSave("ItemListed", async (request) => {
+    //Every event gets triggered twice, once on unconfirmed, again on confirmed
+    const confirmed = request.object.get("confirmed")
+    const logger = Moralis.Cloud.getLogger()
+    logger.info("Looking for confirmed Tx")
+})
