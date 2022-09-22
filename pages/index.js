ef21e673bcd393b5881d673ab05f15eac7e616da
Borislav Stoyanov <bostoy@protonmail.com>
2022-09-22 15:34:51 +0300

    ✨ Fetch Active NFTs from Moralis

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,31 @@
-import Head from "next/head"
+import { list } from "postcss"
+import { useMoralisQuery } from "react-moralis"
 
 export default function Home() {
-    //How do we show the recently listed NFTs?
+    const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
+        "ActiveItem",
+        (query) => query.limit(10).descending("tokenId")
+    )
 
-    //We will index the events off-chain and then read from our database.
-    //Setup a server to listen for those events to be fired, and we will add them to a database to query.
-    //The Graph does this in a decentralized way
-    /** Moralis does it in a centralized way and comes with a ton of other functions
-     * All our logic is still 100% on chain.
-     * Speed & Development time.
-     * Its really hard to start a prod blockchain project 100% decentralized.
-     * They are working on open sourcing their code
-     * Feature richness
-     * We can create more features with a centralized back end to start
-     * AS more decentralized tools are being created
-     * Local development
-     */
-    return <div>Home Page</div>
+    console.log("Listed Nfts: ", listedNfts)
+    return (
+        <div>
+            {fetchingListedNfts ? (
+                <div>Loading...</div>
+            ) : (
+                listedNfts.map((nft) => {
+                    const { price, nftAddress, tokenId, marketplaceAddress, seller } =
+                        nft.attributes
+
+                    return (
+                        <div>
+                            Price: {price} . NftAddress: {nftAddress}. TokenId: {tokenId}. Seller:{" "}
+                            {seller}. Marketplace Address: {marketplaceAddress}
+                        </div>
+                    )
+                })
+            )}
+            Home Page
+        </div>
+    )
 }
